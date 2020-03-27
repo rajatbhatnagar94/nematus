8ef3a5034550ec188896a65a5c77e1c9f75209de
Rajat Bhatnagar <rajatbhatnagar94@gmail.com>
2020-03-27 12:03:58 -0600

    gave shape explicitly in rnn_model

--- nematus/rnn_model.py
+++ nematus/rnn_model.py
@@ -54,11 +54,14 @@ class RNNModel(object):
         dropout_embedding, dropout_hidden = None, None
         if config.rnn_use_dropout and config.rnn_dropout_embedding > 0.0:
             def dropout_embedding(e):
-                return tf.layers.dropout(e, rate=config.rnn_dropout_embedding,
+                shape=(tf.shape(e)[0], tf.shape(e)[1])
+                return tf.layers.dropout(e, noise_shape=shape,
+                                         rate=config.rnn_dropout_embedding,
                                          training=self.inputs.training)
         if config.rnn_use_dropout and config.rnn_dropout_hidden > 0.0:
             def dropout_hidden(h):
-                return tf.layers.dropout(h,
+                shape=(tf.shape(h)[0], tf.shape(h)[1])
+                return tf.layers.dropout(h, noise_shape=shape,
                                          rate=config.rnn_dropout_hidden,
                                          training=self.inputs.training)
 
